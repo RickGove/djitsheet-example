19363645f99a85b9ade7c7b1a38af315b0b4f853
Rick Gove <rickbgove@gmail.com>
2020-08-31 20:50:52 +0300

    attempt 1

---
+++ precache-manifest.5c3ddc50c9436332414501b112ede22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b036ba7698bb76165c5cc64b87eb10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5354b6a1ccb2bc701f22",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "52e0f83567c6a51580b5",
+    "url": "./static/js/2.3740edf7.chunk.js"
+  },
+  {
+    "revision": "e5011e51b8728a17ff9ce6a352869a9b",
+    "url": "./static/js/2.3740edf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5354b6a1ccb2bc701f22",
+    "url": "./static/js/main.e8e0c79e.chunk.js"
+  },
+  {
+    "revision": "3e7fc46acedb0014aedc",
+    "url": "./static/js/runtime-main.a7764269.js"
+  }
+]);
